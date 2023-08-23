296c7265a15a0d5a6f2fd22e217030a81d88f14f
Phrank08 <frankokey469@gmail.com>
2023-08-23 08:01:00 -0700

    Task-1 of my first collaborative project

---
+++ 1-format_specifier.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point of the program.
+ * description - Handling the following format specifier 'd' and 'i'
+ * Author - Eneh Franklyn Okechukwu
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 42;
+	int num1 = 27;
+
+	printf("Number %d and %i are integers\n", num, num1);
+
+	return (0);
+}
